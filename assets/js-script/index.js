566baf01881a967003c169914eac5872e579e13e
Martin Rouy <martinrouy03@gmail.com>
2024-03-16 18:35:10 +0100

    pb with modal display

--- assets/js-script/index.js
+++ assets/js-script/index.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM loaded");
+  const body = document.querySelector("body");
+  // const formDiv = document.querySelector(".formDiv");
+  const form = document.querySelector("form");
+  // console.log(body);
   const loginButton = document.querySelector(".connectYs");
   loginButton.addEventListener("click", (e) => {
-    console.log(loginButton);
-    console.log(e);
-    loginButton.classList.add(".color-red");
+    body.classList.toggle("contactForm");
+    // formDiv.classList.toggle("contactForm");
+    form.style.display = "inherit";
   });
 });
